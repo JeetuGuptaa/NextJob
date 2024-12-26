4f45b04e59cc2376f738bb63316cd71e870cfcda
Jeetu Gupta <jeetu270602@gmail.com>
2024-12-26 20:11:51 +0530

    create get all jobs api

--- routes/v1Routes/jobRoutes.js
+++ routes/v1Routes/jobRoutes.js
@@ -5,10 +5,12 @@ const {
   createJob,
   getJobBySlug,
   isSlugAvailable,
+  getAllJobs,
 } = require("../../controllers/jobController");
 
 router.post("/create", createJob);
-router.get("/:slug", getJobBySlug);
+router.get("/all-jobs", getAllJobs);
 router.get("/verify-slug/:slug", isSlugAvailable);
+router.get("/:slug", getJobBySlug);
 
 module.exports = router;
