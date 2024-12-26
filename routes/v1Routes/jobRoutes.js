3d905fca262633cecd2d47cc5007ef7df4476923
Jeetu Gupta <jeetu270602@gmail.com>
2024-12-26 20:03:23 +0530

    create verify slug

--- routes/v1Routes/jobRoutes.js
+++ routes/v1Routes/jobRoutes.js
@@ -1,9 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-const { createJob, getJobBySlug } = require("../../controllers/jobController");
+const {
+  createJob,
+  getJobBySlug,
+  isSlugAvailable,
+} = require("../../controllers/jobController");
 
 router.post("/create", createJob);
 router.get("/:slug", getJobBySlug);
+router.get("/verify-slug/:slug", isSlugAvailable);
 
 module.exports = router;
